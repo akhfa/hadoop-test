2ca0365a861970e55bd106d8609c6a2b48d0d020
akhfa <akhmadfakhoni@gmail.com>
2015-12-15 08:35:27 +0700

    added parser

---
+++ Parser.java
@@ -0,0 +1,45 @@
+import java.io.*;
+public class Parser
+{
+    public static void main(String [] args)
+    {
+        // The name of the file to open.
+        String fileName = "dblp-short.xml";
+
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = 
+                new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                if(line.contains("author"))
+                {
+                    line = line.substring(8, line.indexOf("</a", 7));
+                    System.out.println(line);
+                }   
+            }   
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
+    }
+}
