313059e33a4649162de3667b7c80d278c556fefc
akhfa <akhmadfakhoni@gmail.com>
2015-12-15 15:39:47 +0700

    ngilangin 1 parameter

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # $1 = nama jar
 # $2 = class
-# $3 = input directory
-# $4 = output directory
-/home/hduser/Application/hadoop-2.7.1/bin/hadoop jar $1 $2 $3 $4
+# $3 = input/output directory
+/home/hduser/Application/hadoop-2.7.1/bin/hadoop jar $1 $2 $3
